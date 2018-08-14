04509d19d21822499a643abe618bd8d0d8ea11e9
Marc Butler <marc@mailworks.org>
2018-08-14 15:25:24 +1000

    Simplify bash configuration

--- common.bash
+++
@@ -1,9 +0,0 @@
-export PS1="\h:\w [\$?]\$ "
-export HISTCONTROL=ignoreboth
-
-alias mv='mv -iv'
-alias cp='cp -iv'
-
-alias pd=pushd
-alias xd=popd
-alias d='dirs -v'
--- marc.bash
+++ marc.bash
@@ -1,10 +1,22 @@
 export PROMPT_DIRTRIM=3
 export PS1="\h:\w [\$?]\$ "
+
+alias mv='mv -i'
+alias cp='cp -i'
+alias pd='pushd'
+alias pd1='pushd +1'
+alias pd2='pushd +2'
+alias xd='popd'
+alias pd1='pushd +1'
+alias pd2='pushd +2'
+alias d='dirs -v'
+alias diff='diff -u'
+
 export EDITOR=mg
 export VISUAL=$EDITOR
-
-export HISTIGNORE='h:p:history:pwd:ls:ls -l:emacs'
-export HISTCONTROL=erasedups
+export IGNOREEOF=3
+export HISTIGNORE='ls:cd'
+export HISTCONTROL=ignoreboth
 export HISTTIMEFORMAT='%F %T  '
 
 if [ $(type -t ls) == "alias" ]; then
@@ -16,5 +28,3 @@ if [[ "$(uname -o)" == "GNU/Linux" ]]; then
     alias pbcopy='xclip -selection clipboard -i'
     alias pbpaste='xclip -selection clipboard -o'
 fi
-
-eval $(opam config env)
