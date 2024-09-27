7638d6f775985c6c1b187882bbfeb63731c5e599
Marc Butler <135883+marcbutler@users.noreply.github.com>
2024-09-27 18:13:51 +1000

    Add keyboard key binding map
    More init.el tweaks

--- init.el
+++ init.el
@@ -1,3 +1,6 @@
+(setq custom-file (expand-file-name (concat user-emacs-directory "custom.el")))
+(load custom-file)
+
 ;; ubuntu 22 does not appear to have the emacs standard themese as
 ;; part of the emacs installation
 (load-theme 'whiteboard)
@@ -6,12 +9,22 @@
 ;; enable numbered backup files.
 (setq make-backup-files t
       backup-by-copying t		; Don't clobber symlinks.
-      delete-by-moving-to-trash t
       version-control t
       delete-old-versions t
+      delete-by-moving-to-trash t
       auto-save-default t
       auto-save-timeout 20		; Seconds.
       auto-save-interval 200	       	; Keystrokes.
       )
 
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(require 'use-package)
+(use-package clingo-mode)
+
+;; Key customizations.
 (global-set-key [f8] 'save-buffer)
+(global-set-key [f6] 'comment-region)
+(global-set-key [(control f6)] 'uncomment-region)
